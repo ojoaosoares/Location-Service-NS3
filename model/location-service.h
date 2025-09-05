8f50d1a00e70dcc1bf044da642c85886ed399931
Leviathan <ojoaosoaressantos@gmail.com>
2025-09-05 15:16:39 -0300

    Refactor: identation.

--- model/location-service.h
+++ model/location-service.h
@@ -1,6 +1,7 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2009 University of Washington
+ * Based on code from ns-3(Copyright(c) 2009 University of Washington)
+ * Modified by João Soares, Brasil, 2025
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -18,7 +19,6 @@
 #ifndef LOCATION_SERVICE_H
 #define LOCATION_SERVICE_H
 
-/* INCLUDES */
 #include "ns3/node.h"
 #include "ns3/node-list.h"
 #include "ns3/ipv4-routing-protocol.h"
@@ -33,25 +33,25 @@
 
 namespace ns3 {
 
-class LocationService : public Object{
+  class LocationService : public Object{
 
-public:
-  virtual Vector GetPosition (Ipv4Address adr) = 0;
-  virtual bool HasPosition (Ipv4Address adr) = 0;
-  virtual bool IsInSearch (Ipv4Address adr) = 0;
+    public:
+      virtual Vector GetPosition(Ipv4Address adr) = 0;
+      virtual bool HasPosition(Ipv4Address adr) = 0;
+      virtual bool IsInSearch(Ipv4Address adr) = 0;
 
-  virtual void SetIpv4 (Ptr<Ipv4> ipv4) = 0;
-  virtual Vector GetInvalidPosition () = 0;
-  virtual Time GetEntryUpdateTime (Ipv4Address id) = 0;
-  virtual void AddEntry (Ipv4Address id, Vector position) = 0;
-  virtual void DeleteEntry (Ipv4Address id) = 0;
-  
-  virtual void Purge () = 0;
-  virtual void Clear () = 0;
+      virtual void SetIpv4(Ptr<Ipv4> ipv4) = 0;
+      virtual Vector GetInvalidPosition() = 0;
+      virtual Time GetEntryUpdateTime(Ipv4Address id) = 0;
+      virtual void AddEntry(Ipv4Address id, Vector position) = 0;
+      virtual void DeleteEntry(Ipv4Address id) = 0;
+      
+      virtual void Purge() = 0;
+      virtual void Clear() = 0;
 
-private:
-  void Start ();
-};
+    private:
+      void Start();
+  };
 }
 #endif
 
